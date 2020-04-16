65f76e0f28b0036c240f733213456c8e64d4cf97
SeanMcelroy97 <seanpetermcelroy@gmail.com>
2020-04-16 20:52:58 +0100

    Added first component which will contain all other components and routing

---
+++ src/components/HealthApp.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+// import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+class HealthApp extends React.Component {
+  render() {
+    return (
+      <div>
+        <div>Hi</div>
+        <div>hi again</div>
+      </div>
+    );
+  }
+}
+
+export default HealthApp;
